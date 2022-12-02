f9c2c4646d3cd4a22ed79aed87ff08f958ac8b58
CHOIWONSEOK <chws900@gmail.com>
2022-12-02 16:24:14 +0900

    cors use commit

--- posts/index.js
+++ posts/index.js
@@ -3,7 +3,9 @@ const express = require('express')
 const { randomBytes } = require('crypto')
 const bodyParser = require('body-parser')
 const app = express()
+const cors = require('cors')
 app.use(bodyParser.json());
+app.use(cors())
 
 const posts = {}
 
