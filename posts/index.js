b131414b35b3a7570df263446268bca57ae05976
CHOIWONSEOK <chws900@gmail.com>
2022-12-05 16:52:28 +0900

    event- bus implemented

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const { randomBytes } = require('crypto')
 const bodyParser = require('body-parser')
+const axios = require('axios')
+
 const app = express()
 const cors = require('cors')
 
@@ -10,12 +12,13 @@ app.use(cors())
 
 const posts = {}
 
+
 app.get('/posts', (req, res) => {
     console.log(posts)
     res.send(posts)
 })
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body
 
@@ -24,7 +27,20 @@ app.post('/posts', (req, res) => {
         id, title
     }
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    })
+
     res.status(201).send(posts[id])
+})
+
+app.post('/events', (req, res) => {
+    console.log('Received Event', req.body.type)
+
+    res.send({})
 })
 
 app.listen(4000, () => {
